588a4b255fd8ab3a73bcf90ac889b72f7e17b812
Diexvel <velardesdiego@gmail.com>
2025-08-31 20:24:41 -0300

    Ejerciio 2

---
+++ TP2/Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author velar
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese el primer número: ");
+        int n1 = sc.nextInt();
+        System.out.print("Ingrese el segundo número: ");
+        int n2 = sc.nextInt();
+        System.out.print("Ingrese el tercer número: ");
+        int n3 = sc.nextInt();
+
+        int mayor = n1;
+        if (n2 > mayor) mayor = n2;
+        if (n3 > mayor) mayor = n3;
+
+        System.out.println("El mayor es: " + mayor);
+
+    }
+    
+}
